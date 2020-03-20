6dc4386d6c600d91e45264520cb1f9bee9585acb
Kyl206187611 <2061876116@qq.com>
2020-03-20 13:42:42 +0800

    这是第一天考试提交

---
+++ app/src/main/java/com/bw/guanxingwang20200320/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bw.guanxingwang20200320;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //这是第一次
+    }
+}
